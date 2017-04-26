92d6146c6830ff892ae66aac074d652645547287
sam_vs6 <samuel-vs6@hotmail.com>
2017-04-26 15:53:30 +0200

    Subiendo prueba 1 de spark

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class Main {
+
+	public static void main(String[] args) {
+		get("/", (request, response) -> hola());
+		post("/hola", (request, response) -> responder_saludo(request.queryParams("nombre_saludo")));
+		get("/Arquitectura", (request, response) -> "Hola PAYRTOLL");
+	}
+
+	private static String responder_saludo(String nombre) {
+		System.out.println("----------PAYROLL---------");
+		return "Hola "+nombre;
+	}
+
+	private static String hola() {
+		return "<html>"
+				+ "<body>"
+				+ "<form method='post' action='/hola'>" 
+				+ "<label>Nombre:</label>"
+				+ "<input type='text' name='nombre_saludo'>"
+				+ "<input type='submit' value='Saluda'"
+				+ "</body>"
+				+ "</html>";
+	}
+}
