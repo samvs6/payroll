f0afeda823ab5003b6d29521668e4abeb71323e3
sam_vs6 <samuel-vs6@hotmail.com>
2017-04-27 22:41:58 -0400

    Bootstrap agregado

--- views/payrollcasestudy/entities/views/EmployeeView.java
+++ views/payrollcasestudy/entities/views/EmployeeView.java
@@ -2,13 +2,14 @@ package payrollcasestudy.entities.views;
 
 public class EmployeeView {
 	public static String hola() {
-		return "<html>"
-				+ "<body>"
-				+ "<form method='post' action='/hola'>" 
-				+ "<label>Nombre:</label>"
-				+ "<input type='text' name='nombre_saludo'>"
-				+ "<input type='submit' value='Saluda'"
-				+ "</body>"
+		Header header = new Header();
+		return 	header.header()
+					+"<body>"
+						+"<form method='post' action='/hola'>" 
+						+ "<label>Nombre:</label>"
+						+ "<input type='text' name='nombre_saludo'>"
+						+ "<input type='submit' class='btn btn-success' value='Saluda'"
+					+ "</body>"
 				+ "</html>";
 	}
 
