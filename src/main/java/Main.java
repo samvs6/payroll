4e59fd8bf0f5b77855bfccb8bdb0b173e9dc155c
Esteban Zurita <estebanzurita316@gmail.com>
2017-04-30 22:25:45 -0400

    Primer avance con velocity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,24 @@
 import static spark.Spark.get;
 import static spark.Spark.post;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import payrollcasestudy.entities.views.EmployeeView;
 import payrollcasestudy.entities.views.Index;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class Main {
 
 	public static void main(String[] args) {
-		get("/", (request, response) -> Index.idexView());
+		
+		get("/", (request, response) -> {
+		      return new ModelAndView(new HashMap(), "templates/hello.vtl");
+		    }, new VelocityTemplateEngine());
+		
+		//get("/", (request, response) -> Index.idexView());
+	
 		get("/createNewEmployee", (request, response) -> EmployeeView.registrationForm());
 		post("/newEmployee", (request, response) -> EmployeeView.createNewEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address")));
 		get("/showEmployee", (request, response) -> EmployeeView.showEmployee());
