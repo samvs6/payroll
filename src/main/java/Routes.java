e6fc905f5d9c3e1a56d149a88a8808d1952101c5
sam_vs6 <samuel-vs6@hotmail.com>
2017-05-03 02:29:59 -0400

    Arreglos visuales

--- src/main/java/Routes.java
+++ src/main/java/Routes.java
@@ -3,19 +3,18 @@ import static spark.Spark.post;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import payrollcasestudy.entities.Employee;
-import payrollcasestudy.entities.views.EmployeeView;
+import payrollcasestudy.entities.views.EmployeePresenter;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
-public class Main {
+public class Routes {
 
 	public static void main(String[] args) {
 		HashMap<String,Object> view = new HashMap<String, Object>();
 		get("/", (request, response) -> {
-		      return new ModelAndView(view, "templates/hello.vtl");
+		      return new ModelAndView(view, "templates/mainPage.vtl");
 		    }, new VelocityTemplateEngine());
 		get("/createNewEmployee", (request, response) -> {
 		      return new ModelAndView(view, "templates/Employee/newEmployee.vtl");
@@ -31,28 +30,24 @@ public class Main {
 		
 		get("/showAllEmployees", (request, response) -> {
 			ArrayList<Employee> employees=new ArrayList<>();
-			employees =EmployeeView.getAllEmployees();
+			employees =EmployeePresenter.getAllEmployees();
 			view.put("employees", employees);
 		      return new ModelAndView(view, "templates/Employee/listingEmployee.vtl");
 		    }, new VelocityTemplateEngine());
 		
 		get("/employees/:id", (request, response) -> {			
 			int employeeId =  Integer.parseInt( request.params(":id"));
-			Employee employee = EmployeeView.getEmployeeFromDB(employeeId);
+			Employee employee = EmployeePresenter.getEmployeeFromDB(employeeId);
 			view.put("employees", employee);
 			return new ModelAndView(view, "templates/Employee/showEmployee.vtl");
 		    }, new VelocityTemplateEngine());
 		
 		
 		post("/newSalariedEmployee", (request, response) -> 
-		EmployeeView.createNewSalariedEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address"),request.queryParams("salary")));
+		EmployeePresenter.createNewSalariedEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address"),request.queryParams("salary")));
 
 		post("/newHourlyEmployee", (request, response) -> 
-		EmployeeView.createNewHourlyEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address"),request.queryParams("hourlyRate")));
+		EmployeePresenter.createNewHourlyEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address"),request.queryParams("hourlyRate")));
 
-		
-		post("/newEmployee", (request, response) -> EmployeeView.createNewEmployee(request.queryParams("id"), request.queryParams("name"), request.queryParams("address")));
-		get("/showEmployee", (request, response) -> EmployeeView.showEmployee());
-		//get("/showAllEmployees", (request, response) -> EmployeeView.showAllEmployees());
 	}
 }
--- views/payrollcasestudy/entities/views/EmployeePresenter.java
+++ views/payrollcasestudy/entities/views/EmployeePresenter.java
@@ -1,7 +1,6 @@
 package payrollcasestudy.entities.views;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -11,44 +10,8 @@ import payrollcasestudy.transactions.Transaction;
 import payrollcasestudy.transactions.add.AddHourlyEmployeeTransaction;
 import payrollcasestudy.transactions.add.AddSalariedEmployeeTransaction;
 
-public class EmployeeView {
+public class EmployeePresenter {
 
-	public static String getData() {
-		
-		return  "<body>"
-					+"<div class='container'>"
-						+"<form method='post' action='/showEmployee'>" 
-						+ "<input type='submit' class='btn btn-success' value='Presioname'"
-					+"</div>"
-					+ "</body>"
-				+ "</html>";
-	}
-
-	public static Object createNewEmployee(String employeeId, String name, String address) {
-		
-		int employeeIdInt = Integer.parseInt(employeeId);
-		Employee employee = new Employee(employeeIdInt,name,address);
-		safeEmployeeInDB(employeeIdInt, employee);
-		return  "<div class='container'>"
-				+ "Registrando a "+name+" Direccion: "+address+" ID: "+employeeId
-				+"<br><br>"
-				+"<a href='/' type='button' class='btn btn-primary'>Inicio</a>"
-				+"</div>"
-				+ "</body>"
-				+ "</html>";
-	}
-	
-	public static Object showEmployee() {
-		int employeeId=1;
-		Employee employee = getEmployeeFromDB(employeeId);
-        return  "<div class='container'>"
-        		+employee.getName().toString()+ " - " + employee.getAddress()
-		        +"<br><br>"
-				+"<a href='/' type='button' class='btn btn-primary'>Inicio</a>"
-				+"</div>"
-				+ "</body>"
-				+ "</html>";
-	}
 	
 	public static String createNewHourlyEmployee(String employeeId, String name, String address, String hourlyRate){
 		int employeeId_int = Integer.parseInt(employeeId);
@@ -64,13 +27,9 @@ public class EmployeeView {
 		double salary_doule = Double.parseDouble(salary);
 		 Transaction addEmployeeTransaction = new AddSalariedEmployeeTransaction(employeeId_int, name, address,salary_doule);
 	        addEmployeeTransaction.execute();
-	        return "Empleado por hora creado"
+	        return "Empleado por asalariado creado"
 	        		+"<br>"
 	        		+"<a href='/' type='button' class='btn btn-primary'>Inicio</a>";
-	}
-	
-	public static void safeEmployeeInDB(int id, Employee employee){
-		PayrollDatabase.globalPayrollDatabase.addEmployee(id, employee);
 	}
 
 	public static ArrayList<Employee> getAllEmployees(){
